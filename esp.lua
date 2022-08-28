693139339d48b07f268fca3f28345f3970d13f1d
TheGuyYallKnow <108369031+TheGuyYallKnow@users.noreply.github.com>
2022-08-29 00:30:45 +0700

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,6 +1,6 @@
 local Players = game:GetService("Players")
 local UIS = game:GetService("UserInputService")
-local keybindlib = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/TheGuyYallKnow/Tusdeptrai/main/EnumBind2String.lua"))()
+local keybindlib = loadstring(game:HttpGetAsync("https://tuhub.site/Anhtucubu/enumbind2string"))()
 local function S2E(str)
 	if str == '' then
 		return nil
@@ -51,43 +51,7 @@ local DataStructure = {
 }
 local Variables = DataStructure
 local file = 'Tushub/Config.json'
-do --// Saving Essential
-	if not isfolder('Tushub') then
-		makefolder('Tushub')
-	end 
-	if not isfile(file) then
-		writefile(file,'[]')
-	end
-	setmetatable(Variables,{
-		__index = function(t,k,v)
-			if not k then 
-				return DataStructure
-			else
-				return DataStructure[k]
-			end
-		end,
-		__newindex = function(t,k,v)
-			DataStructure[k] = v
-
-			--// Saving
-			local get_data = game:service'HttpService':JSONDecode(readfile(file))
-			for i,v in pairs(DataStructure) do
-				get_data[i] = v
-			end
-			writefile(file, game:service'HttpService':JSONEncode(get_data))
-		end,
-	})
-
-	local data_Get = game:GetService('HttpService'):JSONDecode(readfile(file))
-	for i,v in pairs(DataStructure) do
-		if not data_Get[i] then
-			data_Get[i] = v
-			writefile(file,game:GetService('HttpService'):JSONEncode(data_Get))
-		else
-			Variables[i] = v
-		end
-	end
-end
+loadstring(game:HttpGet('https://tuhub.site/Anhtucubu/saving'))()(file,DataStructure,Variables)
 function WTS(pos)
 	local screen = workspace.CurrentCamera:WorldToViewportPoint(pos)
 	return Vector2.new(screen.x, screen.y)
@@ -431,7 +395,7 @@ task.spawn(function()
 															else
 																rounded = stringto
 															end
-															text = text..tostring(TrackInst[TrackValue])
+															text = text..rounded
 														end
 													end
 												end
