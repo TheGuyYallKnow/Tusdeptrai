9e889216f8c5a749603a9f85964739f77568bf53
TheGuyYallKnow <108369031+TheGuyYallKnow@users.noreply.github.com>
2023-04-21 18:41:13 +0700

    Update StringFilter.lua

--- StringFilter.lua
+++ StringFilter.lua
@@ -1,33 +1,11 @@
 _G.nofall = false
+_G.nostun = false
+_G.antibackfire = false
 _G.spidercloakboost = false
-_G.Speed = 250
-_G.ws = 250
 _G.autotrinket = false
 _G.autoingredient = false
 local Nocliping = false
-local WalkSpeed = false
-local Speed = _G.Speed
-local plr = game:GetService("Players").LocalPlayer
-local char = plr.Character
-local realws 
-realws = char.Humanoid.WalkSpeed
-local function onchar (c)
-	print("1")
-	local hrp = c:WaitForChild("HumanoidRootPart",9e9)
-	char = c
-	realws = char.Humanoid.WalkSpeed
-	spawn(function()
-		local chandler = plr.Character:WaitForChild("CharacterHandler",9e9)
-		local remote = chandler:WaitForChild('Remotes',9e9)
-		local fdr = remote:WaitForChild('ApplyFallDamage',9e9)
-		for i,v in next, getconnections(remote.ChildRemoved) do
-			v:Disable()
-		end
-		fdr:Destroy()
-	end)
-end
-onchar(char)
-plr.CharacterAdded:Connect(onchar)
+
 local function notification(text)
 	game.StarterGui:SetCore("SendNotification", {
 		Title = "Peepoo Peepoo",
@@ -36,27 +14,40 @@ local function notification(text)
 	})
 end
 
-local mt = getrawmetatable(game)
-local namecall
-local newnamecall = newcclosure(function(self,...)
-	local args = {...}
-	local method = getnamecallmethod()
-	local b = table.remove(args) -- last value 
-	if _G.nofall == true and  method:lower() == "fireserver" and not checkcaller() and tonumber(b) ~= nil and tostring(self) == "ApplyFallDamage" then
-		return 1
-	end
-	return namecall(self,...)
-end)
-namecall = hookfunction(mt.__namecall, newnamecall)
+do -- // Animation Play Ban Bypass
+	if(not banAnimation or typeof(banAnimation) ~= 'Instance' or not banAnimation:IsA('AnimationTrack')) then
+		local humanoid;
+		for i, v in next, workspace.NPCs:GetChildren() do
+			if(v:FindFirstChildWhichIsA('Humanoid', true)) then
+				humanoid = v.Humanoid;
+			end;
+		end;
+
+		local animation = Instance.new('Animation');
+		animation.AnimationId = 'rbxassetid://4595066903';
+
+		banAnimation = humanoid:LoadAnimation(animation);
+	end;
+
+	local old;
+	old = hookfunction(banAnimation.Play, newcclosure(function(self)
+		if(typeof(self) ~= 'Instance' or not self:IsA('AnimationTrack')) then return old(self); end;
+
+		if(string.find(self.Animation.AnimationId, '4595066903')) then
+			return;
+		end;
+
+		return old(self);
+	end));
+end;
 
 local UIS = game:GetService("UserInputService")
 local OnRender = game:GetService("RunService").RenderStepped
 
 local Player = game:GetService("Players").LocalPlayer
-local Character = char
+local plr = Player
 
 local Camera = workspace.CurrentCamera
-local Root = Character:WaitForChild("HumanoidRootPart")
 
 local C1, C2, C3;
 local Nav = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
@@ -149,6 +140,7 @@ C1 = UIS.InputBegan:Connect(function(Input,typin)
 			end
 			]]
 		elseif Input.KeyCode == Enum.KeyCode.Z then
+			local Character = Player.Character
 			_G.spidercloakboost = not _G.spidercloakboost
 			notification('Spidercloak mode: '..tostring(_G.spidercloakboost))
 			if not Character:FindFirstChild('Boosts'):FindFirstChild('ClimbBoost') then
@@ -160,20 +152,15 @@ C1 = UIS.InputBegan:Connect(function(Input,typin)
 			if _G.spidercloakboost == true then
 				oldClimboost = Character:FindFirstChild('Boosts'):FindFirstChild('ClimbBoost').Value or 0.15
 				Character:FindFirstChild('Boosts'):FindFirstChild('ClimbBoost').Value = 0.65
-
-				local spboost = Character:FindFirstChild('Boosts'):FindFirstChild('SpeedBoost')
-				if not spboost then
-					spboost = Instance.new('NumberValue',Character:FindFirstChild('Boosts'))
-					spboost.Name = 'SpeedBoost'
-					spboost.Value = 4
-				else
-					oldSpboost = spboost.Value
-				end
+				
+				if oldSpboost then return end
+				oldSpboost = Instance.new('NumberValue',Character:FindFirstChild('Boosts'))
+				oldSpboost.Name = 'SpeedBoost'
+				oldSpboost.Value = 4
 			else
 				Character:FindFirstChild('Boosts'):FindFirstChild('ClimbBoost').Value = oldClimboost
-				if not oldSpboost then
-					local spboost = Character:FindFirstChild('Boosts'):FindFirstChild('SpeedBoost')
-					spboost:Destroy()
+				if oldSpboost then
+					oldSpboost:Destroy()
 				end
 			end
 		elseif Input.KeyCode == Enum.KeyCode.N then
@@ -185,6 +172,12 @@ C1 = UIS.InputBegan:Connect(function(Input,typin)
 		elseif Input.KeyCode == Enum.KeyCode.H then
 			_G.nofall = not _G.nofall
 			notification('No fall: '..tostring(_G.nofall))
+		elseif Input.KeyCode == Enum.KeyCode.J then
+			_G.nostun = not _G.nostun
+			notification('No stun: '..tostring(_G.nostun))
+		elseif Input.KeyCode == Enum.KeyCode.K then
+			_G.antibackfire = not _G.antibackfire
+			notification('Anti backfire: '..tostring(_G.antibackfire))
 		end
 	end
 end)
@@ -214,45 +207,44 @@ C2 = UIS.InputEnded:Connect(function(Input)
 end)
 
 local Players = game:GetService("Players")
-local target = nil
+local target,frameto = nil,nil
 local guiname = "LeaderboardGui"
 local function removespaces(str)
 	return str:gsub(" ","")
 end
 local function spectate()
 	game:GetService("Workspace").Camera.CameraType = "Custom"
-	if not Players:FindFirstChild(target) then return end
+	if not Players:FindFirstChild(target) then
+		workspace.CurrentCamera.CFrame = Player.Character.HumanoidRootPart.CFrame*CFrame.new(Vector3.new(0,4,10))
+		workspace.CurrentCamera.CameraSubject = Player.Character.Humanoid
+		return
+	end
 	workspace.CurrentCamera.CFrame = Players:FindFirstChild(target).Character.HumanoidRootPart.CFrame*CFrame.new(Vector3.new(0,4,10))
 	workspace.CurrentCamera.CameraSubject = Players:FindFirstChild(target).Character.Humanoid
 end
 local mouse = plr:GetMouse()
 local way = plr.PlayerGui:FindFirstChild(guiname).MainFrame.ScrollingFrame
-for i,v in pairs(way:GetChildren()) do
-	v.MouseEnter:connect(function()
+
+FrameAdded = function(frame)
+	frame.MouseEnter:connect(function()
 		wait()
-		target = v.Text
-		if string.match(target,"#") then
-			target = target:sub(4)
-			local str = removespaces(target)
-			target = str
-		end
+		target = frame.Name
+		frameto = frame
 	end)
-	v.MouseLeave:connect(function()
+	frame.MouseLeave:connect(function()
 		target = nil
+		frameto = nil
 	end) 
 end
+for i,v in pairs(way:GetChildren()) do
+	FrameAdded(v)
+end
 way.ChildAdded:Connect(function(v)
-	v.MouseEnter:connect(function()
-		wait()
-		target = v.Text
-	end)
-	v.MouseLeave:connect(function()
-		target = nil
-	end) 
+	FrameAdded(v)
 end)
 UIS.InputBegan:Connect(function(input,typin)
 	if typin then return end
-	if input.UserInputType == Enum.UserInputType.MouseButton1 and target ~= nil then
+	if input.UserInputType == Enum.UserInputType.MouseButton1 and target ~= nil and frameto.Text == target then
 		wait()
 		spectate()
 	end
@@ -362,4 +354,266 @@ spawn(function()
 		end
 	end)
 end)
+
+local NostunTab = {'LightAttack','NoJump','NoCharge','ChargeBlock','NoDash','ClimbCooldown','Stun','Action','RecentDash'}
+local onChar = function(char)
+	char.ChildAdded:Connect(function(item)
+		wait()
+		if _G.nostun == true and table.find(NostunTab,item.Name) then
+			item:Destroy()
+		end
+	end)
+end
+onChar(plr.Character)
+plr.CharacterAdded:Connect(function()
+	wait(1)
+	onChar(plr.Character)
+end)
+
+_G.FallionForgive = 5
+_G.spellsNeed = {
+	["Armis"] = {
+		{min = 40, max = 60};
+		{min = 70, max = 80};
+	};
+	["Trickstus"] = {
+		{min = 30, max = 70};
+		{min = 30, max = 50};
+	};
+	["Scrupus"] = {
+		{min = 30, max = 100};
+		{min = 30, max = 100};
+	};
+	["Celeritas"] = {
+		{min = 70, max = 90};
+		{min = 70, max = 80};
+	};
+	["Velo"] = {
+		{min = 70, max = 100};
+		{min = 40, max = 60};
+	};
+	["Ignis"] = {
+		{min = 80, max = 95};
+		{min = 40, max = 60};
+	};
+	["Gelidus"] = {
+		{min = 80, max = 95};
+		{min = 80, max = 100};
+	};
+	["Viribus"] = {
+		{min = 25, max = 35};
+		{min = 60, max = 70};
+	};
+	["Sagitta Sol"] = {
+		{min = 50, max = 65};
+		{min = 40, max = 60};
+	};
+	["Tenebris"] = {
+		{min = 90, max = 100};
+		{min = 40, max = 60};
+	};
+	["Nocere"] = {
+		{min = 70, max = 85};
+		{min = 70, max = 85};
+	};
+	["Hystericus"] = {
+		{min = 75, max = 90};
+		{min = 15, max = 35};
+	};
+	["Shrieker"] = {
+		{min = 30, max = 50};
+		{min = 30, max = 50};
+	};
+	["Verdien"] = {
+		{min = 75, max = 100};
+		{min = 75, max = 85};
+	};
+	["Contrarium"] = {
+		{min = 80, max = 95};
+		{min = 70, max = 90};
+	};
+	["Floresco"] = {
+		{min = 90, max = 100};
+		{min = 80, max = 95};
+	};
+	["Perflora"] = {
+		{min = 70, max = 90};
+		{min = 30, max = 50};
+	};
+	["Manus Dei"] = {
+		{min = 90, max = 95};
+		{min = 50, max = 60};
+	};
+	["Fons Vitae"] = {
+		{min = 75, max = 100};
+		{min = 75, max = 100};
+	};
+	["Trahere"] = {
+		{min = 75, max = 85};
+		{min = 75, max = 85};
+	};
+	["Furantur"] = {
+		{min = 60, max = 80};
+		{min = 60, max = 80};
+	};
+	["Inferi"] = {
+		{min = 10, max = 30};
+		{min = 10, max = 30};
+	};
+	["Howler"] = {
+		{min = 60, max = 80};
+		{min = 60, max = 80};
+	};
+	["Secare"] = {
+		{min = 90, max = 95};
+		{min = 90, max = 95};
+	};
+	["Ligans"] = {
+		{min = 63, max = 80};
+		{min = 63, max = 80};
+	};
+	["Reditus"] = {
+		{min = 50, max = 100};
+		{min = 50, max = 100};
+	};
+	["Fimbulvetr"] = {
+		{min = 86, max = 90};
+		{min = 70, max = 80};
+	};
+	["Gate"] = {
+		{min = 75, max = 85};
+		{min = 75, max = 85};
+	};
+	["Snarvindur"] = {
+		{min = 60, max = 75};
+		{min = 20, max = 30};
+	};
+	["Hoppa"] = {
+		{min = 40, max = 60};
+		{min = 50, max = 60};
+	};
+	["Percutiens"] = {
+		{min = 60, max = 70};
+		{min = 70, max = 80};
+	};
+	["Dominus"] = {
+		{min = 50, max = 100};
+		{min = 50, max = 100};
+	};
+	["Custos"] = {
+		{min = 45, max = 65};
+		{min = 45, max = 65};
+	};
+	["Claritum"] = {
+		{min = 90, max = 100};
+		{min = 90, max = 100};
+	};
+	["Globus"] = {
+		{min = 70, max = 100};
+		{min = 70, max = 100};
+	};
+	["Intermissum"] = {
+		{min = 70, max = 100};
+		{min = 70, max = 100};
+	};
+}
+
+local players = game:service'Players';
+local player = players.LocalPlayer;
+
+local uIP = game:GetService'UserInputService';
+
+local throwns = workspace.Thrown
+
+local remoteNames = {
+	["LeftClick"] = 1; -- normal
+	["RightClick"] = 2; -- snap
+}
+
+local function checkForThrown(spell)
+	local playername = player.Name;
+	if not throwns:FindFirstChild(spell) then return false end;
+	for i,v in pairs(throwns:GetChildren()) do
+		if v.Name == spell then
+			if v.Weld.Part0.Parent.Name == playername then
+				return true;
+			end
+		end
+	end
+	return false
+end
+
+local function checkForTool(toolName)
+	return player.Backpack:FindFirstChild(toolName) or player.Character:FindFirstChild(toolName)
+end
+
+local function getMageClass()
+	if checkForTool("Furantur") or checkForTool("Globus") then return 15 end
+	if checkForTool("Inferi") or checkForTool("Observe") or checkForTool("Perflora") then return 50 end
+	return 100;
+end
+
+local Forgive = {'Contrarium','Ignis','Geldius','Celeritas','Hystericus','Nocere'}
+
+local mt = getrawmetatable(game)
+local namecall
+local newnamecall = newcclosure(function(self,...)
+	local args = {...}
+	local method = getnamecallmethod()
+	local b = table.remove(args) -- last value 
+	if method:lower() == 'fireserver' then
+		if _G.nofall == true and not checkcaller() and tonumber(b) ~= nil and tostring(self) == "ApplyFallDamage" then
+			return 1
+		end
+		local castType = remoteNames[tostring(self)]
+		if _G.antibackfire == true and  then
+			local char = player.Character;
+			if not char then return namecall(self, ...) end;
+			local tool = char:FindFirstChildOfClass'Tool';
+			if not tool then return namecall(self, ...) end;
+			if not tool:FindFirstChild'Spell' then return namecall(self, ...) end;
+			local backpack = player.Backpack;
+			local isFallion = backpack:FindFirstChild'WiseCasting';
+			local spellsNeed = _G.spellsNeed;
+			local spell = tool.Name;
+			local spellInfo = spellsNeed[spell][castType];
+			local min = spellInfo.min;
+			local max = spellInfo.max;
+			local FallionForgive = _G.FallionForgive;
+			local isNotSnap = castType==1;
+			min = (isFallion and isNotSnap) and (min - FallionForgive) or min;
+			max = (isFallion and isNotSnap) and (max + FallionForgive) or max;
+			local mana = char.Mana.Value;
+			if char.Artifacts:FindFirstChild'PhilosophersStone' then
+				if mana > getMageClass() then
+					return namecall(self, ...);
+				end
+			end
+			if table.find(Forgive,tool.Name) and char:FindFirstChild('ActiveCast') then
+				return namecall(self, ...);
+			end
+			if mana > min and mana < max then
+				return namecall(self, ...);
+			else
+				if spell == "Tenebris" and castType == 2 then
+					if checkForThrown("DarkBall") then
+						return namecall(self, ...);
+					end
+				elseif spell == "Gate" then
+					if not char:FindFirstChild'Combat' and char:FindFirstChild'AzaelHorn' then
+						return namecall(self, ...);
+					end
+				elseif spell == "Velo" then
+					if char:FindFirstChild'LightBall' then
+						return namecall(self, ...);
+					end
+				end
+			end
+			return nil;
+		end
+	end
+	return namecall(self,...)
+end)
+namecall = hookfunction(mt.__namecall, newnamecall)
+
 notification("Loaded in!")
